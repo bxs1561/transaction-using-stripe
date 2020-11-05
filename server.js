ca625a520859f8d6f0ff5e3daefa9215c217de05
bikramsubedi <bxs1561@g.rit.edu>
2020-11-05 00:18:06 -0500

    buy and process transaction using stripe

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const stripe = require("stripe")("sk_test_51HjYYuJXnhrs42nh7rBYbIGYoTzd4LAIwvd9HxsJ4hmHopWOu4dUMfC1DWJFgOisl4989drcugri92wL20VqGIhZ00yCNCEu9D");
+const bodyParser = require("body-parser");
+const expressHandlebars = require("express-handlebars");
+
+
+const app = express();
+
+const port = process.env.PORT || 9000;
+
+app.engine("handlebars",expressHandlebars({defaultLayout: "main"}));
+app.set('view engine', 'handlebars');
+
+//body middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+//set static folders to put the images
+app.use(express.static(`${__dirname}/public`))
+
+//charges route
+
+app.post("/charge",(req,res)=>{
+    // console.log(req.body)
+    const amount= 5000;
+    stripe.customers.create({
+        email: req.body.stripeEmail,
+        source:req.body.stripeToken
+    }).then((customer)=>{
+        stripe.charges.create({
+            customer: customer.id,
+            amount: amount,
+            currency: 'usd',
+            description: 'Comfortable clothes',
+
+        })
+    }).then(charge=>res.render("success"))
+})
+//index route
+app.get("/",(req,res)=>{
+    res.render("index",{
+        publishKey:process.env.PUBLISH_KEY
+    })
+});
+
+
+app.listen(port,()=>console.log(`listening to: ${port}`));
